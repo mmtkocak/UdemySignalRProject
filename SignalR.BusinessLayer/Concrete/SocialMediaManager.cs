61e9f9bf50f9e7b734229ffd4834d4daae9d568f
Mehmet KOÇAK <tr.mehmetkocak@gmail.com>
2023-11-21 17:26:00 +0300

    .

---
+++ SignalR.BusinessLayer/Concrete/SocialMediaManager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialmediaDal;
+
+        public SocialMediaManager(ISocialMediaDal socialmediaDal)
+        {
+            _socialmediaDal = socialmediaDal;
+        }
+
+        public void TAdd(SocialMedia entity)
+        {
+            _socialmediaDal.Add(entity);
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socialmediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetByID(int id)
+        {
+            return _socialmediaDal.GetByID(id);
+        }
+
+        public List<SocialMedia> TGetListAll()
+        {
+            return _socialmediaDal.GetListAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialmediaDal.Update(entity);
+        }
+    }
+}
---
+++ SignalRApi/Controllers/BookingController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateBooking(CreateBookingDto createBookingDto)
+        {
+            Booking booking = new Booking()
+            {
+                Date = createBookingDto.Date,
+                Mail = createBookingDto.Mail,
+                Name = createBookingDto.Name,
+                PersonCount = createBookingDto.PersonCount,
+                Phone = createBookingDto.Phone
+            };
+            _bookingService.TAdd(booking);
+            return Ok("Rezervasyon Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            _bookingService.TDelete(value);
+            return Ok("Rezervasyon Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBooking(UpdateBookingDto updateBookingDto)
+        {
+            Booking booking = new Booking()
+            {
+                BookingID = updateBookingDto.BookingID,
+                Date = updateBookingDto.Date,
+                Mail = updateBookingDto.Mail,
+                Name = updateBookingDto.Name,
+                PersonCount = updateBookingDto.PersonCount,
+                Phone = updateBookingDto.Phone
+            };
+            _bookingService.TUpdate(booking);
+            return Ok("Rezervasyon Güncellendi");
+        }
+
+        [HttpGet("GetBooking")]
+        public IActionResult GetBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
